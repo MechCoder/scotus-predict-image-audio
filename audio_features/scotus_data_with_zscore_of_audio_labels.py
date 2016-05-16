e3e99aec7e1bbd12b06b80c3b75f37aef7a56612
MechCoder <manojkumarsivaraj334@gmail.com>
2016-05-16 15:49:34 -0400

    Minor changes

--- audio_features/scotus_data_with_zscore_of_audio_labels.py
+++ audio_features/scotus_data_with_zscore_of_audio_labels.py
@@ -1,8 +1,29 @@
 import pandas as pd
+import os.path
 
-audio_files = pd.read_csv("data/pilot6b_long_only_f_n_outcomes.csv")
-raw_data = pd.read_csv("data/output/remove_features_unknown.csv")
-f_data = pd.read_csv("data/output/remove_data_unknown.csv")
+cols = ['zAggressive', 'zAttractive', 'zConfident', 'zMasculine', 'zWin', 'zQuality', 'zIntel', 'zTrust']
+audio_file_path = "data/input/pilot6b_long_only_f_n_outcomes.csv"
+if not os.path.isfile(audio_file_path):
+    raise ValueError(
+        "We are bounded by a NDA agreement to not share this file. If you have a "
+        "csv file that contains ratings of every case that has the following columns, "
+        "CaseNum, %s, this should work just as well. We are sorry that we cannot make "
+        "the script completely reproducible but we at least have a decent sense of humour."
+        % cols)
+audio_files = pd.read_csv(audio_file_path)
+
+raw_data_path = "data/input/remove_features_unknown.csv"
+if not os.path.isfile(raw_data_path):
+    import zipfile
+    out_path = "data/input"
+    data_zip = open("data/input/data_remove.zip", "rb")
+    z = zipfile.ZipFile(data_zip)
+    for name in z.namelist():
+        z.extract(name, out_path)
+    z.close()
+
+raw_data = pd.read_csv("data/input/remove_features_unknown.csv")
+f_data = pd.read_csv("data/input/remove_data_unknown.csv")
 
 index_P = audio_files["Petitioner"] == "p"
 cols = ['zAggressive', 'zAttractive', 'zConfident', 'zMasculine', 'zWin', 'zQuality', 'zIntel', 'zTrust']
@@ -31,7 +52,7 @@ def check(x):
 audio_ratings["docket"] = audio_ratings["docket"].apply(lambda x: check(x))
 raw_data = raw_data.merge(audio_ratings, on="docket", how="inner")
 raw_data = raw_data.fillna(0.0)
-raw_data.to_csv("data/output/merge_audio_features.csv")
+raw_data.to_csv("data/input/merge_scdb_with_audio_features.csv")
 
 f_data = f_data.merge(audio_ratings, on="docket", how="inner")
-f_data.to_csv("data/output/merge_audio_data.csv")
+f_data.to_csv("data/input/merge_scdb_with_audio_data.csv")

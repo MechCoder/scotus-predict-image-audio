b6f9f8340f77db6ad74f0d00abb4c9e472a25338
MechCoder <manojkumarsivaraj334@gmail.com>
2016-05-17 01:58:57 -0400

    Add modify_advocates.py

---
+++ audio_features/modify_advocates.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import re
+
+def convert_string(x):
+    return ''.join([t.lower() for t in re.split('[, . ]', x)])
+
+advocates = pd.read_csv("data/input/advocates.csv")
+advocates["lawyer_name"] = advocates["lawyer_name"].apply(convert_string)
+advocates.to_csv("data/input/modified_advocates.csv")
